bdaf64500dea57434f43d4c97d850ff2a1757566
Rodrigo Fernández <rodrigo@aquih.com>
2025-02-07 16:12:02 +0000

    Agregar licencia

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'FEL Infile',
-    'version': '1.2',
+    'version': '1.3',
     'category': 'Custom',
     'description': """ Integración con factura electrónica de Infile """,
     'author': 'aquíH',
@@ -13,6 +13,7 @@
         'views/res_partner_views.xml',
     ],
     'demo': [],
-    'installable': True
+    'installable': True,
+    'license': 'Other OSI approved licence',
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

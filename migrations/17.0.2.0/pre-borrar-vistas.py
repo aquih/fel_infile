8ff6a3808b5467d588cac4f4547c2b46c7d09039
Rodrigo Fernández <rodrigo@aquih.com>
2025-03-18 18:46:00 +0000

    Mejor borrar las referencias a las vistas.

--- migrations/17.0.2.0/pre-borrar-vistas.py
+++ migrations/17.0.2.0/pre-borrar-vistas.py
@@ -4,5 +4,5 @@ _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    cr.execute("delete from ir_ui_view where id in (select res_id from ir_model_data where module = 'fel_infile' and model = 'ir.ui.view') and type in ('form', 'tree');")
-    _logger.info("Vistas viejas borradas")
+    cr.execute("delete from ir_model_data where module = 'fel_infile' and model = 'ir.ui.view' and name like '%view%';")
+    _logger.info("Referencias a vistas viejas borradas")

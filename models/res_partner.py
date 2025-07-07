fb873d287042a19c649de3fc979fce61609e4013
Ariana Carrillo <ariana@computadora911.com>
2025-07-07 15:13:04 +0000

    Modificacion de obtencion de datos con sat en POS

--- models/res_partner.py
+++ models/res_partner.py
@@ -30,20 +30,43 @@ class Partner(models.Model):
                 "nit_consulta": vat.replace('-',''),
             }
             r = requests.post('https://consultareceptores.feel.com.gt/rest/action', json=data, headers=headers)
-            logging.warning(r.text)
-            if r and r.json():
-                return r.json()
-                
-        return {'nombre': '', 'nit': ''}
+            logging.info(r.text)
+
+            if r:
+                try:
+                    response = r.json()
+                    return {
+                        'nombre': response.get('nombre'), 
+                        'nit': response.get('nit'), 
+                        'mensaje': response.get('mensaje')
+                    }
+                except Exception as e:
+                    logging.warning(f"Error al procesar json: {e}")
+                    return {
+                        'nombre': '', 
+                        'nit': '', 
+                        'mensaje': 'No se pudo procesar la respuesta json.'
+                    }
+        return {'nombre': '', 'nit': '', 'mensaje': ''}
 
     def _datos_sat_cui(self, company, cui):
         if cui:
-            if company.token_cui and datetime.now(pytz.timezone('America/Guatemala')) < datetime.fromisoformat(json.loads(company.token_cui).get('fecha_de_vencimiento')):
-                token = json.loads(company.token_cui).get('token')
-            else:
-                result_token_generado = self._generar_token(company)                
-                if result_token_generado.get('mensaje', {}): #NO se obtuvo token nuevo, retorna solo el mensaje
-                    return result_token_generado
+            token = ''
+            if company.token_cui:
+                token_cui = json.loads(company.token_cui)
+                vencimiento_token = token_cui.get('fecha_de_vencimiento')
+                ahora = datetime.now(pytz.timezone('America/Guatemala'))
+                if ahora < datetime.fromisoformat(vencimiento_token):
+                    token = token_cui.get('token')
+    
+            if not token:
+                result_token_generado = self._generar_token(company)             
+                if result_token_generado.get('mensaje', {}):
+                    return {
+                        'nombre': '', 
+                        'nit': '', 
+                        'mensaje': result_token_generado.get('mensaje', {})
+                    } 
                 else: 
                     token = result_token_generado['token']
             
@@ -52,12 +75,31 @@ class Partner(models.Model):
                 "cui": cui,
             }
             r = requests.post('https://certificador.feel.com.gt/api/v2/servicios/externos/cui', data=data, headers=headers)
-            logging.warning(r.text)
-            if r is not None and r.json():
-                new_r = r.json()
-                new_r['mensaje'] = new_r.get('descripcion')
-                return new_r
-        return {'nombre': '', 'cui': '', 'mensaje': "No se pudo obtener el CUI."}
+            logging.info(r.text)
+
+            if r is not None:
+                try:
+                    response = r.json()
+                    if response.get('cui', {}) and response.get('cui', {}).get('nombre') != 'NA':
+                        return {
+                            'nombre': response['cui']['nombre'],
+                            'nit': response['cui']['cui'],
+                            'mensaje': ''
+                        }
+                    else:
+                        return {
+                            'nombre': '',
+                            'nit': '',
+                            'mensaje': response.get('descripcion', 'No se encontró descripción')
+                        }
+                except Exception as e:
+                    logging.warning(f"Error al procesar json: {e}")
+                    return {
+                        'nombre': '', 
+                        'nit': '', 
+                        'mensaje': 'No se pudo procesar la respuesta json.'
+                        } 
+        return {'nombre': '', 'nit': '', 'mensaje': "No se pudo obtener el CUI."}
     
     def _generar_token(self, company):
         data_token = {
@@ -65,13 +107,27 @@ class Partner(models.Model):
             "llave": company.clave_fel,
         }
         r_token = requests.post('https://certificador.feel.com.gt/api/v2/servicios/externos/login', data=data_token)
-        logging.warning(r_token.text)
-        if r_token is not None and r_token.json():
-            if r_token.json()['resultado'] == False:
-                return {'mensaje':r_token.json()['descripcion']} 
-            else:
-                company.token_cui = json.dumps(r_token.json())
-                return r_token.json()
-        else:
-            return {'mensaje': "No se pudo obtener el token de autenticación para CUI."}
+        logging.info(r_token.text)
+
+        if r_token is not None:
+            try:
+                token_response = r_token.json()
+                if token_response.get('resultado') is False:
+                    return {
+                        'token': '',
+                        'mensaje': token_response.get('descripcion', 'Sin descripción')
+                    }
+                else:
+                    company.token_cui = json.dumps(token_response)
+                    return {
+                        'token': token_response.get('token', ''),
+                        'mensaje': token_response.get('descripcion', '')
+                    }
+            except Exception as e:
+                logging.warning(f"Error al procesar json: {e}")
+                return {
+                    'token': '',
+                    'mensaje': 'No se pudo procesar la respuesta json.'
+                }
+        return {'token': '', 'mensaje': "No se pudo obtener el token de autenticación para CUI."}
             
